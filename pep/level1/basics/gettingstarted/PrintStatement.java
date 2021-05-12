a4ee79c12e15a25eb892ee15d045f64c2e0632bc
aamirfayaz <dropmeataamir@gmail.com>
2021-05-12 23:30:06 +0530

    level1 pepcoding startec

---
+++ pep/level1/basics/gettingstarted/PrintStatement.java
@@ -0,0 +1,7 @@
+public class PrintStatement {
+  public static void main(String [] args) {
+
+      System.out.print("*\n**\n***\n****\n*****\n");
+
+    }
+}
---
+++ pep/level1/basics/gettingstarted/PrintZ.java
@@ -0,0 +1,17 @@
+public class PrintZ {
+  public static void main(String [] args) {
+    System.out.println("*****");
+    System.out.println("   * ");
+    System.out.println("  *  ");
+    System.out.println(" *   ");
+    System.out.println("*****");
+
+ int a = 10;
+ int b = 20;
+ a = a + b;
+ b = a - b;
+ a = a - b;
+ System.out.println(a + " " + b);
+
+  }
+}
