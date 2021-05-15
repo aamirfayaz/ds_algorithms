16fd0afe161351f48e8962a1a49bf3734f7e1adc
aamirfayaz <dropmeataamir@gmail.com>
2021-05-16 02:25:39 +0530

    inverse face index of a number

--- pep/level1/basics/gettingstarted/InverseOfANumber.java
+++ pep/level1/basics/gettingstarted/InverseOfANumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class InverseOfANumber {
   public static void main(String [] args) {
 
@@ -7,6 +9,24 @@ public class InverseOfANumber {
     if(s.hasNext())
        n = s.nextInt();
 
-         
+
+        int i = 0;
+        int p = 1;
+        while(n > 0){
+            int dig = n % 10;
+            System.out.println("dig = " + dig);
+            n = n / 10;
+            System.out.println("n = " + n);
+            System.out.println("p = " + p);
+            System.out.println("Math.pow(10, dig - 1) = " + Math.pow(10, dig - 1));
+            i += p * Math.pow(10, dig - 1);
+            System.out.println("i = " + i);
+            System.out.println("===========iteration====");
+            p++;
+        }
+
+        System.out.println(i);
+
+
   }
 }
