b237efd2cbe4a0c107bce8ca75d3f08eff55b292
aamirfayaz <dropmeataamir@gmail.com>
2021-05-20 01:24:41 +0530

    cross line starts

--- pep/level1/basics/patterns/Pattern9.java
+++ pep/level1/basics/patterns/Pattern9.java
@@ -1,5 +1,9 @@
 /**
-
+*				*
+	*		*
+		*
+	*		*
+*				*
 **/
 import java.util.Scanner;
 public class Pattern9 {
@@ -12,20 +16,28 @@ public class Pattern9 {
      n = s.nextInt();
 
      solution1(n);
-     
+ }
      static void solution1(int n) {
 
        for(int i = 1; i <= n; i++) {
-         for(int j = i; j <= n; j++) {
-           if(j == n) {
+         for(int j = 1; j <= n; j++) {
+
+          // System.out.println("i = " + i);
+          // System.out.println("j = " + j);
+          //
+          // System.out.println("i + j == n + 1" + (i + j == n + 1));
+          // System.out.println("i == j" + (i == j));
+
+           if(i == j || (i + j == n + 1)) {
              System.out.print("*");
            } else {
-             System.out.print("\t");
-           }
-         }
-         System.out.println();
+               System.out.print("  ");
+             }
+
        }
+       System.out.println();
 
      }
-   }
+
+}
 }
