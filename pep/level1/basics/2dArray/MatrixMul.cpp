27587110fc96c5bade43f010b00ad2cd13039e3b
aamirfayaz <dropmeataamir@gmail.com>
2021-06-17 00:43:09 +0530

    update .in files

--- pep/level1/basics/2dArray/MatrixMul.cpp
+++ pep/level1/basics/2dArray/MatrixMul.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-      int n;
-      cin>>n;
       int m;
       cin>>m;
+      int n;
+      cin>>n;
 
-      int  a[n][m];
+      int a[m][n];
     //  cout<<"\n2D Array Input:\n";
-      for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++) {
+      for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
             //cout<<"\ns["<<i<<"]["<<j<<"]=  ";
-            cin>>a[i][j];
+            cin>>a[m][n];
         }
       }
 
     //  cout<<"\nThe 2-D Array is:\n";
       for(int i=0;i<m;i++) {
         for(int j=0;j<n;j++) {
-            cout<<a[i][j] << " ";
+            cout<<a[m][n] << " ";
         }
         cout<<endl;
       }
