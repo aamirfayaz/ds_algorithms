5d5ede584688f64ded4670d4226746855accda0e
aamirfayaz <dropmeataamir@gmail.com>
2021-06-23 15:04:22 +0530

    updated rotatoe 90 with transpose and reverse

--- pep/level1/basics/2dArray/Rotate90.cpp
+++ pep/level1/basics/2dArray/Rotate90.cpp
@@ -12,16 +12,41 @@ int main() {
 			cin>>a[i][j];
 		}
 	}
-   
-   for(int i=0;i<c;i++) {
-   	 for(int j=r-1;j>=0;j--) {
-   	 	cout << a[j][i] << " ";
-   	 }
-   	 cout << endl;
-   }
 
+	//first transpose the array i.e r -> c
+	for(int i=0;i<r;i++) {
+		for(int j=i;j<c;j++) {
+			int temp = a[i][j];
+			a[i][j] = a[j][i];
+			a[j][i] = temp;
+		}
+	}
 
+	cout << "---------------" << endl;
+	for(int i=0;i<r;i++) {
+		for(int j=0;j<c;j++) {
+			cout<<a[i][j] << " ";
+		}
+		cout << endl;
+	}
 
-	
-	return 0;
+	//now reverse each row of the transposed array
+	for(int i=0;i<r;i++) {
+		int low = 0, high = r-1;
+		while(low<high) {
+			int temp=a[i][low];
+			a[i][low]=a[i][high];
+			a[i][high]=temp;
+			low++;
+			high--;
+		}
+	}
+cout << "---------------" << endl;
+	for(int i=0;i<r;i++) {
+		for(int j=0;j<c;j++) {
+			cout<<a[i][j] << " ";
+		}
+		cout << endl;
+	}
+  	return 0;
 }
