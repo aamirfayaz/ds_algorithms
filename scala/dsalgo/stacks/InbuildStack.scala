1866a4068368dc930d1a638f6164d8d97fdfc3ae
aamirfayaz <dropmeataamir@gmail.com>
2021-08-12 20:24:28 +0530

    system design links added

---
+++ scala/dsalgo/stacks/InbuildStack.scala
@@ -0,0 +1,10 @@
+object InbuildStack extends App {
+
+     import scala.collection.mutable.Stack;
+
+     val st = Stack[Int]()
+     st.push(1)
+     st.push(2)
+     st.push(3)
+     println(st)
+}
