cb10b6f4b5924145fb7abc08411fb1ca6e013698
aamirfayaz <dropmeataamir@gmail.com>
2021-11-06 15:25:33 +0530

    rotate a 2d array by 90

--- pc/2darrays/RotateArrayBy90.java
+++ pc/2darrays/RotateArrayBy90.java
@@ -1,8 +1,60 @@
+import java.util.Scanner;
+
 public class RotateArrayBy90 {
 
  public static void main(String [] args) {
 
- 	 System.out.println("ds and algos");
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int arr [][] = new int[n][n];
+
+     //assuming square matrix
+     for(int i=0;i<arr.length;i++) {
+        for(int j=0;j<arr[0].length;j++) {
+            arr[i][j] = sc.nextInt();
+        }
+     }
+     transpose(arr);
+     reverseRowWise(arr);
+
+     display(arr);
+
+ }
+
+ public static void transpose(int [] [] arr) {
+
+      for(int i=0;i<arr.length;i++) {
+        for(int j=0;j<i;j++) {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+      }  
+ }
+
+ public static void reverseRowWise(int [] [] arr) {
+
+     for(int i=0;i<arr.length;i++) {
+        int li=0, ui = arr[i].length - 1;
+        while(li<ui) {
+          int temp = arr[i][li];
+          arr[i][li] = arr[i][ui];
+          arr[i][ui] = temp;    
+          li++;
+          ui--;
+        } 
+     }
+ }
+
+ public static void display(int [][] arr) {
+
+  for(int i=0;i<arr.length;i++) {
+        for(int j=0;j<arr[0].length;j++) {
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+     }
+
  }
 
 }
