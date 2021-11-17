db5df9bf67390fe4fb1b1eedd2391a0ac85f8354
aamirfayaz <dropmeataamir@gmail.com>
2021-11-17 13:42:20 +0530

    string subsequences

--- pc/recursion/arrays/MaxElementInArray.java
+++ pc/recursion/arrays/MaxElementInArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DisplayArray {
+public class MaxElementInArray {
 
 	public static void main(String [] args) {
 
@@ -12,9 +12,21 @@ public class DisplayArray {
 		for(int i=0; i<a.length;i++) {
 			a[i] = sc.nextInt();
 		}
+
+		int result = max(a, 0);
+		System.out.println(result);
 	}
 
   public static int max(int [] a, int idx) {
-  	
+  	if(idx == a.length)
+  		return Integer.MIN_VALUE;
+  	else {
+
+  		int res = max(a, idx + 1);
+  		if(a[idx] > res)
+  			return a[idx];
+  		else
+  			return res;
+  	}
   }	
 }
